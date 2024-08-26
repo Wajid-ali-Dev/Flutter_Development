fb868d62207110f5d42c1057305ca0964b0778bd
Wajid-ali-Dev <wali30331@gmail.com>
2024-08-26 07:13:20 -0700

    flutter1

--- lib/Week2/list_tile.dart
+++ lib/Week2/list_tile.dart
@@ -2,6 +2,7 @@ import 'dart:js_interop';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:stateless_widgts/Week2/text_from_field.dart';
 
 class ListTilee extends StatelessWidget {
   @override
@@ -16,35 +17,53 @@ class ListTilee extends StatelessWidget {
       "Naseeb"
     ];
     return Scaffold(
-        backgroundColor: Colors.blue[100],
-        appBar: AppBar(
-          title: Text(
-            'tile view'.toUpperCase(),
-            style: TextStyle(
-              letterSpacing: 6,
-              color: Colors.white,
-            ),
+      backgroundColor: Colors.blue[100],
+      appBar: AppBar(
+        title: Text(
+          'tile view'.toUpperCase(),
+          style: TextStyle(
+            letterSpacing: 6,
+            color: Colors.white,
           ),
-          centerTitle: true,
-          backgroundColor: Colors.blue,
         ),
-        body: ListView.separated(
-          itemBuilder: (context, index) {
-            return ListTile(
-              leading: Text('$index'),
-              title: Text(arraynames[index]),
-              subtitle: const Text("Player"),
-              trailing: const Icon(Icons.add),
-            );
-          },
-          itemCount: arraynames.length,
-          separatorBuilder: (context, index) {
-            return Divider(
-              thickness: 2.5,
-              height: 50,
-              color: Colors.black,
-            );
-          },
-        ));
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView.separated(
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Text('$index'),
+            title: Text(arraynames[index]),
+            subtitle: const Text("Player"),
+            trailing: ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue)),
+              child: Text(
+                "Go".toUpperCase(),
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => TextFromField(),
+                    ));
+              },
+            ),
+          );
+        },
+        itemCount: arraynames.length,
+        separatorBuilder: (context, index) {
+          return Divider(
+            thickness: 2.5,
+            height: 50,
+            color: Colors.black,
+          );
+        },
+      ),
+    );
   }
 }
---
+++ lib/Week2/text_from_field.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class TextFromField extends StatelessWidget {
+  var email = TextEditingController();
+  var password = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
+          title: Text(
+            'Text from field'.toUpperCase(),
+            style: TextStyle(
+              letterSpacing: 6,
+              color: Colors.white,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+            child: Container(
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.blue, width: 2),
+              color: Colors.white70,
+              borderRadius: BorderRadius.circular(10)),
+          height: 350,
+          width: 400,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                keyboardType: TextInputType.text,
+                controller: email,
+                decoration: InputDecoration(
+                    hintText: "Enter Your Email",
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Colors.blue,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide:
+                            BorderSide(color: Colors.black87, width: 2)),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide(color: Colors.blue, width: 2))),
+              ),
+              Container(
+                height: 60,
+              ),
+              TextField(
+                obscureText: true,
+                obscuringCharacter: "*",
+                controller: password,
+                decoration: InputDecoration(
+                    hintText: "Enter Your Password",
+                    prefixIcon: Icon(
+                      Icons.key,
+                      color: Colors.blue,
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        Icons.remove_red_eye,
+                        color: Colors.blue,
+                      ),
+                      onPressed: () {},
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide:
+                            BorderSide(color: Colors.black87, width: 2)),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide(color: Colors.blue, width: 2))),
+              ),
+              SizedBox(
+                height: 60,
+              ),
+              ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.blue)),
+                  onPressed: () {
+                    String username = email.text.toString();
+                    String passwordd = password.text;
+                    print("Email: $email, Password: $password");
+                  },
+                  child: Text(
+                    "login".toUpperCase(),
+                    style: TextStyle(color: Colors.white70),
+                  ))
+            ],
+          ),
+        )));
+  }
+}
